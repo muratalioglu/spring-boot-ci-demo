0c4b96e82b8d9dfd52c59fdc144a4d921bc4b8c7
Murat <mdenizalioglu@gmail.com>
2023-10-25 16:13:33 +0300

    Initialize

---
+++ src/main/java/com/example/springbootcidemo/MainController.java
@@ -0,0 +1,14 @@
+package com.example.springbootcidemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/main")
+    public ResponseEntity<?> mainEndpoint() {
+        return ResponseEntity.ok("selam");
+    }
+}
---
+++ src/main/java/com/example/springbootcidemo/SpringBootCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCiDemoApplication.class, args);
+	}
+
+}
